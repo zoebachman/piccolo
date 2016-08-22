4becc80d238721fa3a6d9d76681c10cc7b44be72
Zoe Bachman <zoe.bachman@gmail.com>
2016-08-22 14:50:42 -0400

    added theater style and color wheel

--- Piccolo/piccolo_Test/piccolo_Test.ino
+++ Piccolo/piccolo_Test/piccolo_Test.ino
@@ -185,6 +185,8 @@ void setup() {
            _BV(ADATE) | // Auto trigger
            _BV(ADIE)  | // Interrupt enable
            _BV(ADPS1) | _BV(ADPS0); // 64:1 / 13 = 9615 Hz
+
+          
   TIMSK0 = 0;                // Timer0 off
   sei(); // Enable interrupts
 
@@ -294,4 +296,38 @@ ISR(ADC_vect) { // Audio-sampling interrupt
     sample - 512; // Sign-convert for FFT; -512 to +511
 
   if (++samplePos >= FFT_N) ADCSRA &= ~_BV(ADIE); // Buffer full, interrupt off
+}
+
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
